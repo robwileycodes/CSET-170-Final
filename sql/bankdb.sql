2d820c6c46639cb4eaa66082bf36825c91bd6768
robwileycodes <robertpwileyjr@gmail.com>
2026-04-03 10:21:16 -0400

    added sql file

--- sql/bankdb.sql
+++ sql/bankdb.sql
@@ -9,4 +9,20 @@ CREATE TABLE users (
     phoneNum VARCHAR(20) NOT NULL,
     pw VARCHAR(20) NOT NULL
 );
-SELECT * FROM TABLE;
+SELECT * FROM users;
+INSERT INTO users (fName, lName, SSN, Address, phoneNum, pw)
+VALUES ('Admin', 'Account', '1', '123 Admin Street', 'adminNum', 'admin123');
+ALTER TABLE users MODIFY COLUMN SSN VARCHAR(20);
+ALTER TABLE users ADD balance INT;
+ALTER TABLE users ADD accNUm INT;
+ALTER TABLE users RENAME COLUMN accNUm TO accNum;
+
+ALTER TABLE users ADD COLUMN status VARCHAR(10) NOT NULL DEFAULT 'pending';
+ALTER TABLE users ADD COLUMN isAdmin TINYINT(1) NOT NULL DEFAULT 0;
+UPDATE users SET status = 'approved', isAdmin = 1 WHERE SSN = 'your_admin_SSN';
+SET SQL_SAFE_UPDATES = 0;
+UPDATE users SET status = 'approved' WHERE SSN = '1';
+ALTER TABLE users MODIFY COLUMN accNum VARCHAR(20);
+UPDATE users SET balance = 0 WHERE SSN = '123456';
+
+UPDATE users SET balance = 100 where SSN = '123456';
